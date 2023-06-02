c1cba7ae3b4860b4db14500152e7d6e5580b11ad
Adrian1706 <adrianjbuenop@gmail.com>
2023-06-02 16:41:41 -0500

    fix: :rocket: correcion del punto 1
    un cambio del punto 1

--- array.php
+++ array.php
@@ -1,4 +1,7 @@
 <?php
+    echo <<<HTML
+    Nombre Del Planeta Buscado: <a href="index.html"></a>
+    HTML;
     $planetas = ["Sol"=>1, "Mercurio"=>2, "Venus"=>3, "Tierra"=>4, "Marte"=>5, "Jupiter"=>6, "Saturno"=>7, "Urano"=>8, "Neptuno"=>9, "Pluton"=>10];
     
     if(isset($_POST)){
