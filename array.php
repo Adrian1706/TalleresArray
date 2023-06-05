9a1001838d84c61f8cc65c6540fc015aca19d00d
Adrian1706 <adrianjbuenop@gmail.com>
2023-06-05 17:27:57 -0500

    fix: :fire: Punto 4 incompleto
    punto 4 sin terminar

--- array.php
+++ array.php
@@ -5,7 +5,7 @@
     <a href="index.html">Volver a la pagina anterior</a>
     <br>
     HTML;
-    $array = ["Mercurio"=>true, "Venus"=>true, "Tierra"=>true, "Marte"=>true, "Jupiter"=>true, "Saturno"=>false, "Urano"=>false, "Neptuno"=>false, "Pluton"=>false];
+    $array = ["Sol"=>274, "Mercurio"=>2.7, "Venus"=>8.8, "Tierra"=>9.8, "Marte"=>3.7, "Jupiter"=>22.8, "Saturno"=>9, "Urano"=>7.7, "Neptuno"=>11];
     if(isset($_POST)){
         $planet=$_POST["planets"];
         function habitables($valor)
