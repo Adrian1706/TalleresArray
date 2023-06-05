99b290e0f9213f7d5a92e7d2dcf17392f8a69794
Adrian1706 <adrianjbuenop@gmail.com>
2023-06-05 16:18:59 -0500

    feat: :rocket: Ejercicio 2
    Punto 2 terminado

--- array.php
+++ array.php
@@ -1,12 +1,17 @@
 <?php
     echo <<<HTML
-    Nombre Del Planeta Buscado: <a href="index.html"></a>
+    Planetas: 
+    <br>
+    <a href="index.html">Volver a la pagina anterior</a>
     HTML;
-    $planetas = ["Sol"=>1, "Mercurio"=>2, "Venus"=>3, "Tierra"=>4, "Marte"=>5, "Jupiter"=>6, "Saturno"=>7, "Urano"=>8, "Neptuno"=>9, "Pluton"=>10];
-    
     if(isset($_POST)){
         $search=$_POST["planet"];
-            $planetsO = array_flip($planetas);
-            echo "$planetsO[$search]";
-    };
+        $number_planets = array_fill(0, $search, 'planeta');
+        function imprimir($planeta)
+    {
+        echo "$planeta...\n";
+    }
+    echo"<br>";
+    array_walk($number_planets,'imprimir');
+    }
 ?>
